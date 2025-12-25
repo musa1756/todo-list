605283e809225abb483839a99faab0f794694e2e
Musa <musa1756@proton.me>
2025-12-25 10:01:44 +0300

    add: Project Factory function

---
+++ src/modules/project.js
@@ -0,0 +1,14 @@
+// Factory function for creating Project 
+
+export function createProject(name) {
+
+    const id = crypto.randomUUID();
+    let todos = [];
+
+    return {
+        id,
+        name,
+        todos,
+    }
+
+}
